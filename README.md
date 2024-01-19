27ad6c6f543067cb33ee7053adf29b118f88f6cf
taylor <taylor@msoe.edu>
2024-01-19 09:45:13 -0600

    Week 1 Lecture 3

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 This repository contains code written in Dr. Taylor's lectures
 for CSC1120, Data Structures and Graphical Interfaces.
 
+# Announcements
+
+## Week 1
+- Lab quiz
+- Homework due Sunday
+- Watch intro video on lab if you haven't yet
+- Intermediate commits
+- Available for Teams meetings today: 10-11am and 4-5pm
+- Can be available tomorrow with sufficient notice
+
--- src/wk1/Driver.java
+++ src/wk1/Driver.java
@@ -1,7 +1,25 @@
 package wk1;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
-
+        Path path = Path.of("data/delaysByAirline.txt");
+        try (Scanner in = new Scanner(path)) {
+            in.nextLine();
+            in.nextLine();
+            int numCancelled = in.nextInt();
+            String temp = in.next();
+            int percentCancelled = Integer.parseInt(temp.substring(0, temp.length()-1));
+            int numDelayed = in.nextInt();
+            temp = in.next();
+            int percentDelayed = Integer.parseInt(temp.substring(0, temp.length()-1));
+            String airline = in.nextLine();
+            System.out.println(numCancelled + " flights cancelled on " + airline);
+        } catch (IOException e) {
+            System.out.println("Error reading the file. Terminating...");
+        }
     }
 }
