c5df2c28cd2d836b0ec0da3fc5e1b76627dc5c32
Dr. Taylor <gitlab@taylor.bz>
2024-02-05 09:54:54 -0600

    Week 4 Lecture 1 end of class

--- src/wk3/FileIO.java
+++ src/wk3/FileIO.java
@@ -7,6 +7,20 @@ import java.util.stream.Stream;
 
 public class FileIO {
     public static void main(String[] args) throws IOException {
+        String filename = "data/sections20230829.csv";
+        try (Stream<String> stream = Files.lines(Path.of(filename))) {
+            long count = stream.map(line -> new SectionMeeting(line))
+                    .filter(m -> m.meetsAt("Monday", 16) ||
+                            m.meetsAt("Tuesday", 16) ||
+                            m.meetsAt("Wednesday", 16) ||
+                            m.meetsAt("Thursday", 16) ||
+                            m.meetsAt("Friday", 16))
+                    .count();
+            System.out.println(count);
+        }
+    }
+
+    public static void main2(String[] args) throws IOException {
         String filename = "data/delaysByAirline.txt";
         try (Stream<String> stream = Files.lines(Path.of(filename))) {
             stream.filter(line -> line.charAt(0) != '#')
