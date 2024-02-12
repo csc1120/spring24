8a2e924992d5a9417daee2478efc181e7b3a4b57
Dr. Taylor <gitlab@taylor.bz>
2024-02-12 10:04:53 -0600

    Week 5 Lecture 1

--- src/wk4/ArrayList.java
+++ src/wk4/ArrayList.java
@@ -1,9 +1,6 @@
 package wk4;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class ArrayList<E> implements List<E> {
     private Object[] data;
@@ -24,18 +21,29 @@ public class ArrayList<E> implements List<E> {
     }
 
     @Override
-    public void add(int index, E element) {
-
+    public void add(int index, E element) throws IndexOutOfBoundsException {
+        if (index < 0 || index > size()) {
+            throw new IndexOutOfBoundsException("Stupid index:" + index + " size: " + size());
+        }
+        Object[] biggerGuy = new Object[data.length + 1];
+        for (int i = 0; i < index; i++) {
+            biggerGuy[i] = data[i];
+        }
+        biggerGuy[index] = element;
+        for (int i = index; i < data.length; i++) {
+            biggerGuy[i + 1] = data[i];
+        }
+        data = biggerGuy;
     }
 
     @Override
     public void clear() {
-
+        data = new Object[0];
     }
 
     @Override
-    public boolean contains(Object o) {
-        return false;
+    public boolean contains(Object target) {
+        return indexOf(target) >= 0;
     }
 
     @Override
@@ -44,38 +52,65 @@ public class ArrayList<E> implements List<E> {
     }
 
     @Override
-    public int indexOf(Object o) {
-        return 0;
+    public int indexOf(Object target) {
+        int foundAt = -1;
+        for (int i = 0; foundAt == -1 && i < data.length; i++) {
+            if (Objects.equals(target, data[i])) {
+                foundAt = i;
+            }
+        }
+        return foundAt;
     }
 
     @Override
     public boolean isEmpty() {
-        return true;
+        return data.length == 0;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException("Stupid index:" + index + " size: " + size());
+        }
+        Object[] smallerGuy = new Object[data.length - 1];
+        for (int i = 0; i < index; i++) {
+            smallerGuy[i] = data[i];
+        }
+        for (int i = index; i < smallerGuy.length; i++) {
+            smallerGuy[i] = data[i + 1];
+        }
+        E elementWeRemoved = (E) data[index];
+        data = smallerGuy;
+        return elementWeRemoved;
     }
 
     @Override
-    public boolean remove(Object o) {
-        return false;
+    public boolean remove(Object target) {
+        int index = indexOf(target);
+        if (index >= 0) {
+            remove(index);
+        }
+        return index >= 0;
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException("Stupid index:" + index + " size: " + size());
+        }
+        E soonToBeReturned = (E) data[index];
+        data[index] = element;
+        return soonToBeReturned;
     }
 
     @Override
     public int size() {
-        return 0;
+        return data.length;
     }
 
     @Override
     public Object[] toArray() {
-        return null;
+        return Arrays.copyOf(data, data.length);
     }
 
     @Override
