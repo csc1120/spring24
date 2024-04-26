098bc4047d77d4bc281f332243e4f6e4eb9e1fdc
Dr. Taylor <gitlab@taylor.bz>
2024-04-26 09:51:55 -0500

    Week 14 Lecture 3

---
+++ src/wk14/Driver.java
@@ -0,0 +1,14 @@
+package wk14;
+
+public class Driver {
+    public static void main(String[] args) {
+        MaxHeap heap = new MaxHeap();
+        final int size = 5;
+        for (int i = 0; i < size; i++) {
+            heap.add((int) (Math.random() * size));
+        }
+//        while (!heap.isEmpty()) {
+//            System.out.print(heap.extractMax() + " ");
+//        }
+    }
+}
---
+++ src/wk14/MaxHeap.java
@@ -0,0 +1,48 @@
+package wk14;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MaxHeap {
+    private ArrayList<Integer> values;
+    public MaxHeap() {
+        values = new ArrayList<>();
+    }
+
+    private int parent(int index) {
+        return (index - 1) /2;
+    }
+
+    private int left(int index) {
+        return 2 * index + 1;
+    }
+
+    private int right(int index) {
+        return left(index) + 1;
+    }
+
+    public boolean isEmpty() {
+        return values.isEmpty();
+    }
+
+    public int size() {
+        return values.size();
+    }
+
+    public void add(int value) {
+        values.add(value);
+        int index = size() - 1;
+        while (index > 0 && value > values.get(parent(index))) {
+            Collections.swap(values, index, parent(index));
+            index = parent(index);
+        }
+    }
+
+    public int extractMax() {
+
+    }
+
+    public int getMax() {
+        return values.getFirst();
+    }
+}
